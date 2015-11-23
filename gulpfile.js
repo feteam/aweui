6d9cdcf38189fa938280f655829a220faad2cf82
wangchi <hiwangchi@gmail.com>
2015-11-23 11:42:03 +0800

    scaffold

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,36 @@
   var gulp = require('gulp');
   var connect = require('gulp-connect');
   var less = require('gulp-less');
+  var CleanCSS = require('less-plugin-clean-css');
+
+  var cleanCSS = new CleanCSS({
+    advanced: true
+  });
+
   var paths = {
     root: './',
+    less: 'less/',
+    css: 'css/',
+    js: 'js/',
+    tpl: 'tpl/',
     example: 'example/'
   };
 
-  // gulp.tast('less', function () {
-  //   gulp
-  // });
+  gulp.task('less', function () {
+    gulp.src(paths.less + 'global.less')
+      .pipe(less({
+        plugins: [cleanCSS]
+      }))
+      .pipe(gulp.dest(paths.css))
+      .pipe(connect.reload());
+
+    gulp.src(paths.less + 'page/**/*.less')
+      .pipe(less({
+        plugins: [cleanCSS]
+      }))
+      .pipe(gulp.dest(paths.css + 'page/'))
+      .pipe(connect.reload());
+  });
 
   gulp.task('connect', function () {
     connect.server({
@@ -21,12 +43,19 @@
   });
 
   gulp.task('html', function () {
-    gulp.src(paths.example + '**/*.html')
+    gulp.src(paths.tpl + '**/*.html')
+      .pipe(connect.reload());
+  });
+
+  gulp.task('js', function () {
+    gulp.src(paths.js + '**/*.js')
       .pipe(connect.reload());
   });
 
   gulp.task('watch', function () {
-    gulp.watch([paths.example + '**/*.html'], ['html']);
+    gulp.watch([paths.tpl + '**/*.html'], ['html']);
+    gulp.watch([paths.less + '**/*.less'], ['less']);
+    gulp.watch([paths.js + '**/*.js'], ['js']);
   });
 
   gulp.task('default', ['connect', 'watch']);
